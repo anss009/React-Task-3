1f871561666b4b133c8a0372418ccc888acecd19
Muhammad Anss <163612823+anss009@users.noreply.github.com>
2025-11-08 00:21:19 +0200

    updated

--- my_app/src/App.jsx
+++ my_app/src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
-import Login from './components/Login';
-import SignUp from './components/SignUp';
+import Home from './components/Home.jsx';
+import Login from './components/Login.jsx';
+import SignUp from './components/SignUp.jsx';
 import './App.css';
 
 function App() {
